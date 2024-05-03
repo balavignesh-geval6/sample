aaf51d6f49582a945148f7458191f6d8b9b18cfb
balavignesh-geval6 <157635328+balavignesh-geval6@users.noreply.github.com>
2024-05-03 16:33:36 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+
+import sys
+
+print(sys.version)
+
+if 5 > 2:
+    print("five is grater than two")
+
+if 5 > 2:
+    print('Five is grater than two')
+
+print("Hello, World!")
